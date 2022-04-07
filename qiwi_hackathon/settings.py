f279c409f850b77d70593e59ee372446857afd91
Uliana Koneva <loka.koneva@gmail.com>
2022-04-07 17:16:57 +0500

    Первичная настройка для авторизации и аутентификации.

--- qiwi_hackathon/settings.py
+++ qiwi_hackathon/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-
 ]
 
 MIDDLEWARE = [
@@ -51,6 +50,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware'
 ]
 
 ROOT_URLCONF = 'qiwi_hackathon.urls'
