2fc72d6e2458ca5f3b92086e911dcd81cb0e7198
Uliana Koneva <loka.koneva@gmail.com>
2022-04-07 15:14:27 +0500

    Первая версия базы данных.

--- users/models.py
+++ users/models.py
@@ -1,8 +1,53 @@
+import uuid
+from decimal import Decimal
+
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# from django.db import models
+
+STATUS = [
+    ("0", "0"),
+    ("1", "1")
+]
+
+class UIDMixin(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, blank=True, verbose_name='UID')
+
+    class Meta:
+        abstract = True
+
+
+class Service(UIDMixin):
+    name = models.CharField(max_length=150, verbose_name="Название услуги", null=True, blank=True)
+    cost = models.DecimalField(verbose_name="Цена услуги", default=Decimal(0.00),
+                               max_digits=8, decimal_places=2)
+    company = models.ForeignKey("Company", on_delete=models.SET_NULL,
+                                verbose_name="Организация", null=True, blank=True)
+
+
+class Payment(UIDMixin):
+    sum = models.DecimalField(verbose_name="Стоимость", decimal_places=2, max_digits=8)
+    service_request = models.OneToOneField("ServiceRequest", on_delete=models.SET_NULL,
+                                           null=True, verbose_name="Заявка")
+
+
+class Company(UIDMixin):
+    name = models.CharField(max_length=150, verbose_name="Организация", null=True, blank=True)
+
 
 class User(AbstractUser):
-    pass
+    company = models.ForeignKey("Company", verbose_name="Обслуживающая организация",
+                                on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class ServiceRequest(UIDMixin):
+    user = models.OneToOneField("User", verbose_name="Заказчик",
+                                on_delete=models.SET_NULL, null=True)
+    service = models.ForeignKey("Service", verbose_name="Услуги",
+                                on_delete=models.SET_NULL, null=True)
+    status = models.CharField(choices=STATUS, default=STATUS[0], max_length=150)
+    grade = models.IntegerField(verbose_name="Оценка")
+    comment = models.CharField(max_length=1024, verbose_name="Отзыв", blank=True, null=True)
+    file = models.FileField(verbose_name="Файлы", blank=True, null=True)
 
 
